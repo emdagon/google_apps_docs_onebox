6e073615fb4fa2bdccbaecc83503c0d996944b15
Emilio Daniel González <emdagon@gmail.com>
2017-07-21 10:58:49 -0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -11,18 +11,13 @@ module Onebox
 
       include Engine
 
-      def self.supported_endpoints
-        %w(spreadsheets document forms presentation)
-      end
-
-      # matches_regexp /^(https?:)?\/\/(docs\.google\.com)\/(?<endpoint>(#{supported_endpoints.join('|')}))\/d\/((?<key>[\w-]*)).+$/
-      matches_regexp /^\[embed\-(?<endpoint>(#{supported_endpoints.join('|')}))\=(?<key>[\w-]*)\]$/
+      # matches_regexp /^(https?:)?\/\/(docs\.google\.com)\/(?<endpoint>(spreadsheet|document|presentation|form))\/d\/((?<key>[\w-]*)).+$/
+      matches_regexp /^\[embed\-(?<endpoint>(spreadsheet|document|presentation|form))\=(?<key>[\w-]*)\]$/
       
       def data
         result = { 
-                   title: "Google #{shorttype.to_s.capitalize}",
-                   description: "This #{shorttype.to_s.chop.capitalize} is private",
-                   type: shorttype
+                   title: "",
+                   description: ""
                  }
         if document?
           result[:html] = "<iframe class='gdocs-onebox document?-onebox' src='https://docs.google.com/document/d/#{key}/pub?embedded=true' style='border: 0' width='800' height='600' frameborder='0' scrolling='yes' ></iframe>"
@@ -30,27 +25,26 @@ module Onebox
           result[:html] = "<iframe class='gdocs-onebox spreadsheet-onebox' src='https://docs.google.com/spreadsheet/ccc?key=#{key}&usp=sharing&rm=minimal' style='border: 0' width='800' height='600' frameborder='0' scrolling='yes' ></iframe>"
         elsif presentation?
            result[:html] = "<iframe class='gdocs-onebox presentation-onebox' src='https://docs.google.com/presentation/d/#{key}/embed?start=false&loop=false&delayms=3000' frameborder='0' width='960' height='749' allowfullscreen='true' mozallowfullscreen='true' webkitallowfullscreen='true'></iframe>"
-        elsif forms?
+        elsif form?
           result[:html] = "<iframe class='gdocs-onebox forms-onebox' src='https://docs.google.com/forms/d/#{key}/viewform?embedded=true' width='760' height='500' frameborder='0' marginheight='0' marginwidth='0' scrolling='yes'>Loading...</iframe>"
         end
         result
       end
 
-      def self.short_types
-        @shorttypes ||= {
-          spreadsheets: :sheets,
-          document: :docs,
-          presentation: :slides,
-          forms: :forms,
-        }
-      end
-      
-      def doc_type
-        @doc_type ||= match[:endpoint].to_sym
+      def spreadsheet?
+        match[:endpoint] == 'spreadsheets'
       end
 
-      def shorttype
-        GoogleAppsDocsOnebox.short_types[doc_type]
+      def document?
+        match[:endpoint] == 'document'
+      end
+
+      def presentation?
+        match[:endpoint] == 'presentation'
+      end
+
+      def form?
+        match[:endpoint] == 'forms'
       end
 
       def key
