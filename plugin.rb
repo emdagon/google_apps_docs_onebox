d91b7f198b072b0b0d76dbbb82b8cb2aa92e24da
Emilio Daniel González <emdagon@gmail.com>
2017-07-21 10:25:12 -0300

    Trying with a new regex

--- plugin.rb
+++ plugin.rb
@@ -15,8 +15,9 @@ module Onebox
         %w(spreadsheets document forms presentation)
       end
 
-      matches_regexp /^(https?:)?\/\/(docs\.google\.com)\/(?<endpoint>(#{supported_endpoints.join('|')}))\/d\/((?<key>[\w-]*)).+$/
-
+      # matches_regexp /^(https?:)?\/\/(docs\.google\.com)\/(?<endpoint>(#{supported_endpoints.join('|')}))\/d\/((?<key>[\w-]*)).+$/
+      matches_regexp /^\[embed\-(?<endpoint>(xx|yy))\=(?<key>[\w-]*)\]$/
+      
       def data
         result = { 
                    title: "Google #{shorttype.to_s.capitalize}",
@@ -24,14 +25,13 @@ module Onebox
                    type: shorttype
                  }
         if document?
-          result[:link] = "https://docs.google.com/document/d/#{key}/pub?embedded=true"
+          result[:html] = "<iframe class='gdocs-onebox document?-onebox' src='https://docs.google.com/document/d/#{key}/pub?embedded=true' style='border: 0' width='800' height='600' frameborder='0' scrolling='yes' ></iframe>"
         elsif spreadsheet?
-          result[:link] = "https://docs.google.com/spreadsheet/ccc?key=#{key}&usp=sharing&rm=minimal"
+          result[:html] = "<iframe class='gdocs-onebox spreadsheet-onebox' src='https://docs.google.com/spreadsheet/ccc?key=#{key}&usp=sharing&rm=minimal' style='border: 0' width='800' height='600' frameborder='0' scrolling='yes' ></iframe>"
         elsif presentation?
-          result[:link] = "https://docs.google.com/presentation/d/#{key}/embed?start=false&loop=false&delayms=3000"
+           result[:html] = "<iframe class='gdocs-onebox presentation-onebox' src='https://docs.google.com/presentation/d/#{key}/embed?start=false&loop=false&delayms=3000' frameborder='0' width='960' height='749' allowfullscreen='true' mozallowfullscreen='true' webkitallowfullscreen='true'></iframe>"
         elsif forms?
-          result[:link] = "https://docs.google.com/forms/d/#{key}/viewform?embedded=true"
-        end
+          result[:html] = "<iframe class='gdocs-onebox forms-onebox' src='https://docs.google.com/forms/d/#{key}/viewform?embedded=true' width='760' height='500' frameborder='0' marginheight='0' marginwidth='0' scrolling='yes'>Loading...</iframe>"
         result
       end
 
